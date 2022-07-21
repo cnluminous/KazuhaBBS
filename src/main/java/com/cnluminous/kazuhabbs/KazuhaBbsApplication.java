5339705349eb77b4535a961e79c1d27e7da7d0c4
CNLuminous ASUS <admin@cnluminous.com>
2022-07-21 15:30:01 +0800

    初始化项目

---
+++ src/main/java/com/cnluminous/kazuhabbs/KazuhaBbsApplication.java
@@ -0,0 +1,13 @@
+package com.cnluminous.kazuhabbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KazuhaBbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KazuhaBbsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cnluminous/kazuhabbs/KazuhaBbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnluminous.kazuhabbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KazuhaBbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
